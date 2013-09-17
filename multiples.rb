2780cd3f0ab4e9fd136605fc98002f023f852aab
Desmond Rawls <captaingrover@gmail.com>
2013-09-17 17:56:32 -0400

    solve summing multiples less than x for two factors

---
+++ multiples.rb
@@ -0,0 +1,60 @@
+class Multiples
+
+  attr_reader :limit
+
+  def initialize(limit, *factors)
+    @limit = limit
+    @factors = factors
+  end
+
+  def better_sum
+    sum_factors(@factors) - sum_lcm(@factors)
+  end
+
+  def sum_factors(factors)
+    total = 0
+    factors.each do |factor|
+      total += sum_factor(factor)
+    end
+    total
+  end
+
+  def sum_factor(factor)
+    factor_total = 0
+    co_factor = 1
+    while co_factor <= self.multiples(factor)
+      factor_total += co_factor * factor
+      co_factor += 1
+    end
+    factor_total
+  end
+
+  def multiples(factor)
+    multiples = (self.limit - 1) / factor
+    multiples.to_i
+  end
+
+  def sum_lcm(factors)
+    total = 0
+    co_factor = 1
+    lcm = lcm(factors)
+    while co_factor <= self.lcm_multiples(lcm)
+      total += co_factor * lcm
+      co_factor += 1
+    end
+    total
+  end
+
+  def lcm(factors)
+    lcm = factors.inject(:lcm)
+  end
+
+
+  def lcm_multiples(lcm)
+    lcm_multiples = (self.limit - 1) / lcm
+    lcm_multiples.to_i
+  end
+end
+
+ten = Multiples.new(1000, 3, 5, 7)
+puts ten.better_sum
